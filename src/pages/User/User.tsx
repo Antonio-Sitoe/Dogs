391825e61fb8f6e72af12d2c8ce40073022dc905
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-08-12 11:22:59 +0200

    update

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Feed from "../../Components/Feed/Feed";
 import { AnimeLeft } from "../../styles/GlobalStyle";
--- src/pages/User/UserStats.tsx
+++ src/pages/User/UserStats.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 function UserStats() {
-  return (
-    <div>UserStats</div>
-  )
+  return <div>UserStats</div>;
 }
 
-export default UserStats
+export default UserStats;
