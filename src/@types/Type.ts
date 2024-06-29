f35f489ca656090aaccc90b66d9d3da244edf260
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2024-06-29 19:47:28 +0200

    ajustes

--- src/@types/Type.ts
+++
@@ -1,8 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-import { IPhotoGet, IUserStatisData } from "./interfaces";
-
-export type SetModalPhoto = Dispatch<SetStateAction<IPhotoGet | null>>;
-
-export type IFeedPhotosGetList = Array<IPhotoGet>;
-
-export type UserStatisArrayData = Array<IUserStatisData>;
